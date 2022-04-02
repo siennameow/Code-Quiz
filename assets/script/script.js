21c4d7a5197607b9a4617acb13ae00b721b70981
Hexuan Li <lihexuan1@gmail.com>
2022-04-02 14:40:49 -0700

    show question pages

--- assets/script/script.js
+++ assets/script/script.js
@@ -19,6 +19,7 @@ var startBtn = document.querySelector("#start_button");
 var questionPage = document.querySelector("#question_page");
 var askQuestion = document.querySelector("#ask_question");
 
+var reactButtons = document.querySelectorAll(".choices");
 var answerBtn1 = document.querySelector("#answer_btn1");
 var answerBtn2 = document.querySelector("#answer_btn2");
 var answerBtn3 = document.querySelector("#answer_btn3");
@@ -26,42 +27,42 @@ var answerBtn4 = document.querySelector("#answer_btn4");
     //Define questions (Object)
 var questionSource = [
     {
-        question: "String values must be enclosed within _____ when being assigned to variables.",
+        question: "Questions 1 : String values must be enclosed within _____ when being assigned to variables.",
         choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
         answer: "c. quotes"
     },
     {
-        question: "Commonly used data types DO NOT include:",
+        question: "Questions 2 : Commonly used data types DO NOT include:",
         choices: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
         answer: "c. alerts"
     },
     {
-        question: "How do you create a function in JavaScript",
+        question: "Questions 3 : How do you create a function in JavaScript",
         choices: ["a. function = myFunction()", "b. function myFunction()", "c. function:myFunction()", "d. createMyFunction()"],
         answer: "b. function myFunction()"
     },
     {
-        question: "How do you call a function named myFunction?",
+        question: "Questions 4 : How do you call a function named myFunction?",
         choices: ["a. call myFunction()", "b. call function myFunction()", "c. myFunction()", "d. call myFunction"],
         answer: "c. myFunctions()"
     },
     {
-        question: "To see if two variables are equal in an if / else statement you would use ____.",
+        question: "Questions 5 : To see if two variables are equal in an if / else statement you would use ____.",
         choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
         answer: "b. =="
     },
     {
-        question: "The first index of an array is ____.",
+        question: "Questions 6 : The first index of an array is ____.",
         choices: ["a. 0", "b. 1", "c. 8", "d. any"],
         answer: "a. 0"
     },
     {
-        question: "How to write an IF statement in JavaScript?",
+        question: "Questions 7 : How to write an IF statement in JavaScript?",
         choices: ["a. if i == 5 then", "b. if i = 5 then", "c. if(i == 5)", "d. if i = 5"],
         answer: "c. if(i == 5)"
     },
     {
-        question: "Which event occurs when the user clicks on an HTML element?",
+        question: "Questions 8 : Which event occurs when the user clicks on an HTML element?",
         choices: ["a. onclick", "b. onchange", "c. onmouseover", "d. onmouseclick"],
         answer: "a. onclick"
     }
@@ -71,7 +72,7 @@ var questionSource = [
 var timeLeft = document.getElementById("timer");
 
 var secondsLeft = 100;
-// var questionCount = 0;
+var questionCount = 0;
 /*Functions*/
     //WHEN I click the start button, THEN a timer starts(The setInterval() Method)
 function countdown() {
@@ -96,27 +97,44 @@ function countdown() {
 function startQuiz () {
         introPage.style.display = "none";
         questionPage.style.display = "block";
-        questionCount = 0;
+        questionCount = 0
         countdown();
         showQuestion(questionCount);
       
 }
-
-function showQuestion (num) {
-    if (num < questionSource.length) {
-        askQuestion.textContent = questionSource[num].question;
-        answerBtn1.textContent = questionSource[num].choices[0];
-        console.log(answerBtn1);
-        answerBtn2.textContent = questionSource[num].choices[1];
-        answerBtn3.textContent = questionSource[num].choices[2];
-        answerBtn4.textContent = questionSource[num].choices[3];
+//present the questions
+// function showQuestion (n) {
+//     console.log("hello");
+//     if (n < questionSource.length) {
+//         askQuestion.textContent = questionSource[n].question;
+//         answerBtn1.textContent = questionSource[n].choices[0];
+//         answerBtn2.textContent = questionSource[n].choices[1];
+//         answerBtn3.textContent = questionSource[n].choices[2];
+//         answerBtn4.textContent = questionSource[n].choices[3];
+//         questionCount = questionCount + 1;
+//     }
+// }
+function showQuestion (n) {
+        askQuestion.textContent = questionSource[n].question;
+        answerBtn1.textContent = questionSource[n].choices[0];
+        answerBtn2.textContent = questionSource[n].choices[1];
+        answerBtn3.textContent = questionSource[n].choices[2];
+        answerBtn4.textContent = questionSource[n].choices[3];
+        questionCount = n;
     }
 
-    // questionPage.style.display = "block";
-    // answerBtn1.style.display = "block";
-    // questionPage.style.display = "block";
-    // questionPage.style.display = "block";
+
+function nextQuestion () {
+        showQuestion(questionCount+1);
 }
+
+//currentBtn.addEventListener('click', showNextSlide);
+reactButtons.forEach(function(click){
+
+    click.addEventListener("click", nextQuestion);
+})
+
+
     //WHEN I answer a question,Show if answer is correct or wrong 
     //THEN I am presented with another question
     //WHEN all questions are answered or the timer reaches 0, Game is over
